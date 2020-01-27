c0375b957a11d6b793e904685b5a70db2f922bbe
Alex LaFroscia <alex@lafroscia.com>
2020-01-27 15:45:48 -0500

    feat: configure Storybook as a dev environment

---
+++ stories/Color/text.stories.js
@@ -0,0 +1,10 @@
+export default {
+  title: "Color|Text",
+  decorators: [storyFn => `<div style="margin: 16px">${storyFn()}</div>`]
+};
+
+export const ChangeOnHover = () => `
+  <p class="text-red hover:text-blue">
+    Hover me for blue text!
+  </p>
+`;
