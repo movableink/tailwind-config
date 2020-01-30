a5f547fa10a7d2438d689b863c4eba986c4fd47e
Alex LaFroscia <alex@lafroscia.com>
2020-01-30 14:49:02 -0500

    fix: drop `fontWeight` besides `bold`
    Following discussion today, it was decided that we only need to define
    a single weight (besides `normal`), `bold`, which would use the `600`
    weight that is recommended by the brand team (instead of the default
    `700`).
    
    This allows developers to ignore the fact that we technically never want
    to use "bold", and instead always want "semibold".
    
    We also style the semantic `b` and `strong` elements with the correct
    weight.

--- plugins/base.js
+++ plugins/base.js
@@ -6,6 +6,10 @@ module.exports = function fluidTailwindPlugin({ addBase, theme }) {
       fontFamily: theme('fontFamily.sans').reduce((acc, font) => `${acc}, ${font}`)
     },
 
+    'b, strong': {
+      fontWeight: theme('fontWeight.bold')
+    },
+
     'input, textarea': {
       '&::placeholder': {
         color: theme('colors.neutral.500')
