3d7dbec0db101d31f727c3ea5a9f01927c08b552
Alex LaFroscia <alex@lafroscia.com>
2020-01-30 15:53:30 -0500

    refactor: rename story factory function

--- stories/Utilities/Font/line-height.stories.js
+++ stories/Utilities/Font/line-height.stories.js
@@ -3,7 +3,7 @@ import makeCallout from '../../_utils/callout';
 
 const lorem = new LoremIpsum();
 
-function makeLetterSpacingStory(size, notes) {
+function makeLineHeightStory(size, notes) {
   return `
     <h2><pre>.leading-${size}</pre></h2>
     ${notes ? makeCallout(notes) : ''}
@@ -17,36 +17,36 @@ export default {
 };
 
 export const ExtraExtraSmall = () =>
-  makeLetterSpacingStory('2xs', 'Use sparingly. Caption text only.');
+  makeLineHeightStory('2xs', 'Use sparingly. Caption text only.');
 
-export const ExtraSmall = () => makeLetterSpacingStory('xs', 'Use sparingly. Caption text only.');
+export const ExtraSmall = () => makeLineHeightStory('xs', 'Use sparingly. Caption text only.');
 
-export const Small = () => makeLetterSpacingStory('sm', 'Use sparingly. Caption text only.');
+export const Small = () => makeLineHeightStory('sm', 'Use sparingly. Caption text only.');
 
-export const Base = () => makeLetterSpacingStory('base', 'Default line height for all type');
+export const Base = () => makeLineHeightStory('base', 'Default line height for all type');
 
-export const Medium = () => makeLetterSpacingStory('md', 'Used for the largest body styles');
+export const Medium = () => makeLineHeightStory('md', 'Used for the largest body styles');
 
 export const Large = () =>
-  makeLetterSpacingStory(
+  makeLineHeightStory(
     'lg',
     'Reserved for the smaller heading styles. Should never be applied to body text.'
   );
 
 export const ExtraLarge = () =>
-  makeLetterSpacingStory(
+  makeLineHeightStory(
     'xl',
     'Reserved for the smaller heading styles. Should never be applied to body text.'
   );
 
 export const ExtraLarge2 = () =>
-  makeLetterSpacingStory(
+  makeLineHeightStory(
     '2xl',
     'Reserved for the larged heading styles. Should never be applied to body text.'
   );
 
 export const ExtraLarge3 = () =>
-  makeLetterSpacingStory(
+  makeLineHeightStory(
     '3xl',
     'Reserved for the largest heading styles. Should never be applied to body text.'
   );
