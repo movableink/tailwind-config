3722fc48329ccf79573deb5ced904075f2e1e60d
Alex LaFroscia <alex@lafroscia.com>
2020-02-14 11:22:14 -0500

    feat: enable max/min height/width plugins
    The max- and min- height and width plugins from Tailwind actually are
    not configured based on a scale at all. Because of this, it's really
    unlikely that we would have an opinion on what sizes are made available
    to use with them. They should be safe for us to enable even before
    we have the actual height/width size scale defined.

--- tests/no-size-classes.js
+++ tests/no-size-classes.js
@@ -18,10 +18,7 @@ async function parse(filePath) {
 test('there are no height classes', async t => {
   const ast = await parse('../dist/fluid-tailwind.css');
   const rules = ast.nodes.filter(node => node.type === 'rule');
-  const heightRules = rules.filter(
-    ({ selector }) =>
-      selector.startsWith('.h-') || selector.startsWith('.max-h-') || selector.startsWith('.min-h-')
-  );
+  const heightRules = rules.filter(({ selector }) => selector.startsWith('.h-'));
 
   t.is(heightRules.length, 0);
 });
@@ -29,10 +26,7 @@ test('there are no height classes', async t => {
 test('there are no width classes', async t => {
   const ast = await parse('../dist/fluid-tailwind.css');
   const rules = ast.nodes.filter(node => node.type === 'rule');
-  const widthRules = rules.filter(
-    ({ selector }) =>
-      selector.startsWith('.w-') || selector.startsWith('.max-w-') || selector.startsWith('.min-w-')
-  );
+  const widthRules = rules.filter(({ selector }) => selector.startsWith('.w-'));
 
   t.is(widthRules.length, 0);
 });
