82acb43cb2ce443fc243f4e4ef1261630033b9fa
Alex LaFroscia <alex@lafroscia.com>
2020-05-27 21:22:08 -0400

    chore: remove rendering hack for code snippets
    This was necessary when we were rendering code examples as part of our stories, but now that we’re using the an addon for this, we can remove it.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,5 @@
 import { addDecorator, addParameters } from '@storybook/html';
 import { withHTML } from '@whitespace/storybook-addon-html/html';
-import { html } from 'htm/preact';
 import render from 'preact-render-to-string';
 import prettierConfig from '@movable/prettier-config';
 
@@ -14,13 +13,10 @@ addDecorator((storyFn) => {
   let result = storyFn();
 
   if (typeof result === 'string') {
-    result = html([result]);
+    return result;
   }
 
-  const rendered = render(result);
-
-  // Avoid printing serialization code for `<`
-  return rendered.replace(/&amp;/g, '&');
+  return render(result);
 });
 
 addDecorator(withHTML({ prettier: prettierConfig }));
